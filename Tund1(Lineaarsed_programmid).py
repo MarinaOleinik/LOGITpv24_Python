0b841ca3f221307727863695748139a30d011f88
marinaoleinik <marina.oleinik@tthk.ee>
2025-02-25 15:46:28 +0200

    Ülesanded 1-5

---
+++ Tund1(Lineaarsed_programmid).py
@@ -0,0 +1,27 @@
+from random import * # importeerime kõok funktsioonid, mis on random moodulis
+from math import *
+# #Ülesanne 1
+# nimi=input("Mis on sinu nimi?: ")
+# vanus=int(input("Kui vana sa oled?: "))
+
+# print(f"Tere, maailm! Tervitan sind {nimi} Sa oled {vanus} aastat vana.")
+# print("Tere, maailm! Tervitan sind",nimi,"Sa oled",vanus,"aastat vana.")
+# print("Tere, maailm! Tervitan sind"+nimi+"\nSa oled"+str(vanus)+"aastat vana.")
+# #Ülesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(f"Muutuja {vanus} on {type(vanus)} tüübi")
+# print(f"Muutuja {eesnimi} on {type(eesnimi)} tüübi")
+
+#Ülesanne 3
+kommidearv=randint(1,100)
+print(f"Laual on {kommidearv} komme")
+kommidvõtmud=int(input("Mitu kommi tahad ära võtta?"))
+onjäänud=kommidearv-kommidvõtmud
+print(f"Laual on jäänud {onjäänud} komme")
+
+#Ülesanne 4
+
+#Ülesanne 5
