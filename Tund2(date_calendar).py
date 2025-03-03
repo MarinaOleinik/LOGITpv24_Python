659e3e97cd0df7d220dfab9c7d9f02df3c167ef4
marinaoleinik <marina.oleinik@tthk.ee>
2025-03-03 11:47:41 +0200

    Lihtsad programmid

--- Tund2(date_calendar).py
+++ Tund2(date_calendar).py
@@ -1,6 +1,57 @@
 from datetime import *
 from calendar import *
+from math import *
 from time import strptime
+#Ülesanne 10
+minutid_kasutajalt=int(input("Aeg minutides:"))
+tunnid=minutid_kasutajalt//60 # täisosa
+minutid=minutid_kasutajalt%60 # jääk
+print("vastus".center(20,"*")) # vorminda ise vastus nii: "hh:mm"
+
+#Ülesanne 7,8,9 ise
+#Ülesanne 6
+t="""
+Rong see sõitis tsuhh tsuhh tsuhh,
+piilupart oli rongijuht.
+Rattad tegid rat tat taa,
+rat tat taa ja tat tat taa.
+Aga seal rongi peal,
+kas sa tead, kes olid seal?
+
+Rong see sõitis tuut tuut tuut,
+piilupart oli rongijuht.
+Rattad tegid kill koll koll,
+kill koll koll ja kill koll kill.
+"""
+print(t.upper())
+
+#Ülesanne 5
+a="kill-koll ".capitalize()
+b="killadi-koll ".capitalize()
+print(2*a,b,2*a,b,4*a)
+#Ülesanne 4
+#ise
+
+#Ülesanne 3
+
+try:
+    R=float(input("Sisesta R,kus R on ringi raadius:"))
+    # ==, !=, <, >, <=, >=
+    if R<=0:
+        print("Nulliga ja neg. arvudega ei ole mõtte töötada!")
+    else:
+        Ringi_S=round(pi*R**2,2)
+        Ringi_P=round(2*pi*R,2)
+        Ruudu_S=2*R*2*R
+        Ruudu_P=4*2*R
+        print(f"Ringi_S= {Ringi_S}\nRingi_P= {Ringi_P}\nRuudu_S= {Ruudu_S}\nRuudu_P= {Ruudu_P}")
+except:
+    print("Sisesta ainult arvud!!!")
+
+
+
+
+
 tana=date.today()
 print(f"Tere! Täna on {tana}")
 
