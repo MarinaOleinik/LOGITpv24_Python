4c60776c82dcebe0446d69d1fb69d7ac31c8951e
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2025-05-05 09:32:10 +0300

    matplotlib

--- Teema7_txt.py
+++ Teema7_txt.py
@@ -3,19 +3,33 @@ from http import server
 from multiprocessing import context
 import smtplib,ssl
 from email.message import EmailMessage
+import imghdr
 def saada_kiri():
     kellele=input("Kellele: ")
-    kiri="Tere, see on test"
+    kiri="""<!DOCTYPE html>
+<head>
+</head>
+<body>
+<h1>Sending an HTML email from Python</h1>
+<h2>Sending an HTML email from Python</h2>
+<p>Hello there,</p>
+<a href="https://inspirezone.tech/">Here's a link to an awesome dev
+community!</a>
+</body>
+</html>"""
     smtp_server="smtp.gmail.com"
     smtp_port=587
     kellelt="oleinik.marina@gmail.com"
     parool=input("Rakenduste parool") #"agtd qnqq nrkc uotf"
     context=ssl.create_default_context()
     msg=EmailMessage()
-    msg.set_content(kiri)
+    msg.set_content(kiri,subtype="html")
     msg['Subject']="Test"
     msg['From']=kellelt
     msg['To']=kellele
+    with open("coconut.png","rb") as f:
+        pilt=f.read()  
+    msg.add_attachment(pilt,maintype="image",subtype=imghdr.what(None, pilt),filename="coconut.png")
     try:
         server=smtplib.SMTP(smtp_server,smtp_port)
         server.starttls(context=context)
@@ -31,19 +45,30 @@ def Loe_failist(fail:str)->list:
     f=open(fail,'r',encoding="utf-8-sig")
     jarjend=[]
     for rida in f:
-        jarjend.append(rida.strip())
+        k,v=rida.strip()
+        jarjend.append(v)
     f.close()
     return jarjend
+import ast
+def Loe_failist_1(fail:str)->list:
+    with open('loetelu.txt', 'r', encoding='utf-8-sig') as f:
+        for rida in f:
+            rida = rida.strip()
+            if rida:  # Kui rida ei ole tühi
+                andmed=eval(rida)
+                vaartused = list(andmed.values())
+                print(vaartused)
+    
 def Kirjuta_failisse(fail:str,jarjend:list):
     f=open(fail,'w',encoding="utf-8-sig")
     for line in jarjend:
         f.write(line+'\n')
     f.close()
 
-loetelu=Loe_failist('Loetelu.txt')
+loetelu=Loe_failist_1('Loetelu.txt')
 print(loetelu)
-for i in range(8,11,1):
-    loetelu.append(input(f"{i}-i:"))
-Kirjuta_failisse('Loetelu.txt',loetelu)
-loetelu=Loe_failist('Loetelu.txt')
-print(loetelu)
+# for i in range(8,11,1):
+#     loetelu.append(input(f"{i}-i:"))
+# Kirjuta_failisse('Loetelu.txt',loetelu)
+# loetelu=Loe_failist('Loetelu.txt')
+# print(loetelu)
---
+++ Teema8.py
@@ -0,0 +1,32 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.arange(-10,10,1) #-10,-9,-8....10 linspace(-10,10,100) punktide arv
+y=2*x**2-4*x+5
+plt.figure(facecolor="yellow")
+plt.plot(x,y)
+plt.xlabel('X')
+plt.ylabel('y=2*x**2-4*x+5')
+plt.title('Graafik')
+plt.grid()
+plt.savefig('graafik.png')
+plt.show()
+
+
+x=[1, 2, 3, 4, 5]
+y=[10, 20, 25, 30, 35]
+plt.plot(x, y,linestyle="--",marker="D",markersize=10,color="red")
+plt.xlabel('X-telg')
+plt.ylabel('Y-telg')
+plt.title('Lihtne graafik')
+plt.show()
+
+plt.scatter(x, y, color='lightblue',label="Punktidest diagramm")
+plt.legend()
+plt.show()
+
+plt.bar(x, y, color='orange',label="Tulpdiagramm")
+plt.legend()
+plt.show()
+#hist() 	Histogramm
+#pie() 	Sektordiagramm
